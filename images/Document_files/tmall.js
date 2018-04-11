1d3ef4bc254075131b97cf5b1cf87626bf6f06b0
zhuhuijun <724536439@qq.com>
2018-04-11 16:00:04 +0800

    sad

---
+++ images/Document_files/tmall.js
@@ -0,0 +1,21 @@
+// //我的淘宝下拉
+// var myTaobao = $('.my-taobao');
+// var menu1 = $('#menu-1');
+//
+// myTaobao.mouseover(function() {
+//     menu1.show()
+// });
+// myTaobao.mouseout(function() {
+//     menu1.hide()
+// });
+//
+// //收藏夹下拉
+// var collect = $('.collect');
+// var menu2 = $('#menu-2');
+//
+// collect.mouseover(function() {
+//     menu2.show()
+// });
+// collect.mouseout(function() {
+//     menu2.hide()
+// });
---
+++ js/tmall.js
@@ -0,0 +1,53 @@
+//左边側栏按下变色
+// $(function(){
+//   $('.color_type_pink').click(function(){
+//     $('.color_type_pink').addClass('active_pink');
+//     $('.color_type_blue').removeClass('active_blue');
+//
+//   });
+//
+//   $('.color_type_blue').click(function(){
+//     $('.color_type_blue').addClass('active_blue');
+//     $('.color_type_pink').removeClass('active_pink');
+//   });
+//
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var colorTypeBlue = $('.color_type_blue');
+// var colorTypePink = $('.color_type_pink');
+// var colorTypeYellow = $('.color_type_yellow');
+//
+//   colorTypeBlue.click(function(){
+//     colorTypeBlue.css('background', 'blue');
+//     colorTypeBlue.siblings().css('background', '#555');
+//   })
+//   colorTypePink.click(function(){
+//     colorTypePink.css('background', 'pink');
+//     colorTypePink.siblings().css('background', '#555');
+//   })
+//   colorTypeYellow.click(function(){
+//     colorTypeYellow.css('background', 'yellow');
+//     colorTypeYellow.siblings().css('background', '#555');
+//   })
